e1be571cc3c1267d21ef7c8d03d6a4ae40731e17
Van <tavan.it@gmail.com>
2022-01-28 23:52:16 +0100

    Update handlerErrors.js
    test remove 123

--- config/handlerErrors.js
+++ config/handlerErrors.js
@@ -4,7 +4,7 @@ Object.values(err.errors).forEach((data) => {
       //console.log(data.properties);
       errs[data.properties.path] = data.properties.message
       })
-      console.log(errs); 123
+      console.log(errs);
       return errs
 }
 module.exports = {handlerError}
